cbb4dccbf6ee8b56e80ee9299802ebf688546c2a
Eliakim Nobre <eliakimnobre@gmail.com>
2020-07-01 11:48:42 -0300

    App

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
-
+ 
 app.use(express.json());
 app.use(cors());
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-const app = require("./app");
+const app = require("./App");
 
 app.listen(3333);
